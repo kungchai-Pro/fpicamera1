f11c0d098f286668095af44f199c431596a0e282
kungchai <kungchaiprompugdee@gmail.com>
2019-05-27 23:23:19 +0700

    add new app

--- App.js
+++ App.js
@@ -40,7 +40,7 @@ export default class App extends Component {
       <View style={styles.container}>
         <Text style={{ fontSize: 18, color: 'red' }}>{this.state.Imagedata}</Text>
 
-                <Button
+        <Button
           onPress={() =>this.UpfilePicture(this.state.Imagedata)}
           title="Press Me"
         />
