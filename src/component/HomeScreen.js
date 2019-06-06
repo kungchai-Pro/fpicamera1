8a241e7acf6f8375d17612a9a9aa30f5a08fa319
kungchai <kungchaiprompugdee@gmail.com>
2019-06-07 00:33:39 +0700

    set up step 004

--- src/component/HomeScreen.js
+++ src/component/HomeScreen.js
@@ -6,7 +6,7 @@ export default class HomeScreen extends Component{
         return(
             <View>
                 <Text>
-                       ทดสอบ load file  ใหนการ ใช้ Github   003
+                       ทดสอบ load file  ใหนการ ใช้ Github   004
                 </Text>
             </View>
         )
